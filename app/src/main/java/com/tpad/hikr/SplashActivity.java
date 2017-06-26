368b4c9a3265ee0d9ace9265b7bd81097101539b
Tolu Olubode <toluwanimiolubode@gmail.com>
2017-06-25 22:39:52 -0400

    Changes to splash page mechanism

--- app/src/main/java/com/tpad/hikr/SplashActivity.java
+++ app/src/main/java/com/tpad/hikr/SplashActivity.java
@@ -1,17 +1,25 @@
 package com.tpad.hikr;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tpad.hikr.R;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        Intent intent = new Intent(this, MainNavActivity.class);
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        Intent intent = new Intent(SplashActivity.this, MainNavActivity.class);
         startActivity(intent);
-        finish();
     }
 }
