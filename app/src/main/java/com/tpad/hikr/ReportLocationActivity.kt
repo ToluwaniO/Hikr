59b20cbc0f2ba67f6c519d5b5f0b2a04c56bf692
ToluwaniO <ogunsanyatoluwani@gmail.com>
2017-08-22 09:28:09 -0400

    Report location activity

---
+++ app/src/main/java/com/tpad/hikr/ReportLocationActivity.kt
@@ -0,0 +1,25 @@
+package com.tpad.hikr
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.tpad.hikr.Fragments.ReportLocationFragment
+import com.tpad.hikr.Fragments.ReviewFragment
+
+import com.tpad.hikr.R
+
+class ReportLocationActivity : AppCompatActivity(), ReportLocationFragment.ReportListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report_location)
+    }
+
+    override fun reportClicked(report: String) {
+        Log.d(LOG_TAG, report)
+    }
+
+    companion object {
+        private val LOG_TAG = ReportLocationActivity::class.java.simpleName
+    }
+}
